8da57de4f844d13059d24a51db7d5a0833417707
David Ganthier <david.ganthier@gmail.com>
2016-08-18 01:23:06 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,14 @@
+// Original call me function.
+/*function callMe() {
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+  var lyric = "maybe";
+}*/
+
+//My solution to the problem.
 function callMe() {
   var lyric = "maybe";
   console.log("I just met you...");
@@ -7,12 +18,22 @@ function callMe() {
   return lyric;
 }
 
-
-function crazy() {
+// Original crazy function
+/*function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
   var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+}*/
+
+//My solution to the problem.
+function crazy() {
+  // fix the code in here:
+  thisIsCrazy();
+
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
