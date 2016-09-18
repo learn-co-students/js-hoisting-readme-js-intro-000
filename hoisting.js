e32988284ba96493b6d1183573292b349796eb94
Aaron Kirschner <aaron.kirschner@gmail.com>
2016-09-18 18:04:50 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,31 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  // fix the code in here:
+  thisIsCrazy();
+
+
 }
 
 function sayMyName() {
-  // fix the code in here:
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+
+  }  // fix the code in here:
   var name = "Cricky";
 
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+
 }
