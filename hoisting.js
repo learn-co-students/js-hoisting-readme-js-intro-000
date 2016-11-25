3ce6dba0f2191132f6d3d13fc277146f116e0982
Ignas nomatter <parazitaz@gmail.com>
2016-11-25 10:19:22 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,36 @@
 function callMe() {
+  var lyric;
+  lyric= "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+    var fix= thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+
 
   sayMy();
 
   function sayMy() {
+    var name;
+    name= "Kristin";
     console.log(name);
-    var name = "Kristin";
+
   }
+  var name = "Cricky";
 }
