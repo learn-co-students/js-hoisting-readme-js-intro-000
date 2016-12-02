50a99c3d450a0d9ac24e892a6c217f90f954a876
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2016-12-02 13:41:48 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,44 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
+var thisIsCrazy = crazy()
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+  console.log("hey!!!")
+  return thisIsCrazy
 }
+
+//function crazy() {
+  // fix the code in here:
+//  thisIsCrazy();
+
+//  var thisIsCrazy = function (){
+//    console.log("hey!!!")
+//  }
+//}
+
+var name = "Kristin"
+var sayMy = sayMyName()
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+  console.log(name)
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
+
+//function sayMyName() {
+  // fix the code in here:
+//  var name = "Cricky";
+
+//  sayMy();
+
+//  function sayMy() {
+//    console.log(name);
+//    var name = "Kristin";
+//  }
+//}
