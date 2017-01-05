74729ea12e1396b4684177cfc53a01becb5ec8a2
Jeremy Wang <jwm191@gmail.com>
2017-01-05 02:47:39 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,33 @@
+//1
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
+//2
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+function thisIsCrazy() {
     console.log("hey!!!")
   }
 }
 
+//3
+
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
   sayMy();
 
   function sayMy() {
+  var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+
   }
 }
