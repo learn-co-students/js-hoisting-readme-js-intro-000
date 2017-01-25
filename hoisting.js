e8242dc4bfc2a1799ad58c408bac21b125eadbeb
Tiago Lopes <tiagolopes489@gmail.com>
2017-01-25 02:51:53 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,6 @@
 function callMe() {
+  var lyric = "maybe";
+  return lyric
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -9,21 +11,22 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+// fix the code in here:
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy()
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+
 
   sayMy();
 
   function sayMy() {
+    var name = "Kristin"
     console.log(name);
     var name = "Kristin";
   }
