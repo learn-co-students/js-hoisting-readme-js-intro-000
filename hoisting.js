a3d61c375f55e2c80064bfe0e5166d40ed07f899
Joshua Jenkins <joshjenkins6@yahoo.com>
2017-02-13 02:52:58 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,20 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+    console.log("I just met you...");
+    console.log("and this is crazy..");
+    console.log("but here's my number..");
+    console.log("so call me");
+    var lyric = "maybe";
+    return lyric;
 }
-
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  // thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -25,6 +25,8 @@ function sayMyName() {
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+    var other = "Kristin";
+    console.log(other);
   }
+
 }
