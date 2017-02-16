0ad20abdb7df061d1fd45f17248ce5969925c9c9
Michal Vacek <vahyno@gmail.com>
2017-02-16 22:59:04 +0000

    Done.

--- test/hoisting-test.js
+++ test/hoisting-test.js
@@ -12,39 +12,39 @@ chai.use(spies)
 
  const expect = chai.expect
 
-//describe('hoisting', () => {
-//  jsdom({
-//    src: fs.readFileSync(path.resolve(__dirname, '..', 'hoisting.js'), 'utf-8')
-//  })
+describe('hoisting', () => {
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'hoisting.js'), 'utf-8')
+  })
 
-//  describe('callMe', () => {
-//    it("returns the string 'maybe'", () => {
-//      expect(callMe()).to.equal("maybe");
-//    })
-//  })
+  describe('callMe', () => {
+    it("returns the string 'maybe'", () => {
+      expect(callMe()).to.equal("maybe");
+    })
+  })
 
-//  describe('loggers', () => {
-//    beforeEach(() => {
-//      chai.spy.on(console, 'log')
-//    })
+  describe('loggers', () => {
+    beforeEach(() => {
+      chai.spy.on(console, 'log')
+    })
 
-  //  afterEach(() => {
-  //    console.log.reset()
-  //  })
+  afterEach(() => {
+      console.log.reset()
+    })
 
-  //  describe('crazy', () => {
-  //    it("prints 'hey!!!' to the console from the nested function", () => {
-  //      crazy()
+    describe('crazy', () => {
+      it("prints 'hey!!!' to the console from the nested function", () => {
+        crazy()
 
-    //    expect(console.log).to.have.been.called.with("hey!!!")
-  //    })
-  //  })
+        expect(console.log).to.have.been.called.with("hey!!!")
+      })
+    })
 
-//    describe('sayMyName', () => {
-//      it("prints 'Kristin' to the console from the nested function", () => {
-//        sayMyName()
-//        expect(console.log).to.have.been.called.with("Kristin")
-//      })
-//    })
-//  })
-//})
+    describe('sayMyName', () => {
+      it("prints 'Kristin' to the console from the nested function", () => {
+        sayMyName()
+        expect(console.log).to.have.been.called.with("Kristin")
+      })
+    })
+  })
+})
