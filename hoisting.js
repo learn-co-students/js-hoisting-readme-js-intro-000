ad66247dbff1ae73da0f7f229a1b1e4c2cdb3e7d
Melanie Mohn <melaniemohn@yahoo.com>
2017-03-11 22:38:41 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,5 @@
 var lyric = "maybe";
-
-function callMe(lyric) {
+function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
