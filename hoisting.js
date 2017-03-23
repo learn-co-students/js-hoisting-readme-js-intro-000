7a4b86ff6e1ef428b1b663469b347b5a7ca2b1fd
Kavindra Wanniarachchi <kavindra_w@hotmail.com>
2017-03-23 09:56:36 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,16 +3,18 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+var lyric = "maybe";
+
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
-  function callMe(){
+  // fix the code in here:
   thisIsCrazy();
-}
-  var thisIsCrazy = function (){
+
+  var thisIsCrazy = function crazy(){
     console.log("hey!!!")
   }
 }
