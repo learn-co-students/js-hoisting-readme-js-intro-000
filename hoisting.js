4247ab3cbda38ac292634f9fa31a732358634be6
Kavindra Wanniarachchi <kavindra_w@hotmail.com>
2017-03-23 09:59:45 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -12,11 +12,12 @@ var lyric = "maybe";
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function crazy(){
+
+  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -26,7 +27,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
+
     var name = "Kristin";
+    console.log(name);
   }
 }
