5e255c49ee29a60c68867d2e4d9dbd3841093250
Stacey Neathery <neathery.stacey@gmail.com>
2017-03-27 04:12:04 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,5 @@
+/* Here is the preliminary lesson (ie all the broken code)
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
@@ -27,4 +29,37 @@ function sayMyName() {
     console.log(name);
     var name = "Kristin";
   }
+}
+*/
+
+//And now for Stacey's fixing all the things!
+function callMe() {
+  var lyric = "maybe"; // var lyric declaration was hoisted, but not the assignment. I moved the assignment up to top of function.
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+}
+
+
+function crazy() {
+  var thisIsCrazy = function (){ // var thisIsCrazy declaration was hoised, but not the assignment. I moved the assignment up and added a semi-colon after the function runs but inside the curly bracket.
+    console.log("hey!!!");
+  }
+    thisIsCrazy();
+}
+
+
+
+function sayMyName() {
+  function sayMy() { // I moved assigning the function to top.
+    var name = "Kristin"; // I moved assigning the var name above logging
+    console.log(name);
+  }
+  var name = "Cricky";
+
+  sayMy();
+
+
 }
