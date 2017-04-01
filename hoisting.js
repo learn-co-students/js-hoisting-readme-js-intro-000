f4f9b905926cb320a512b12efd3abc2d6b6b078a
Hong Choat <hchoat1@gmail.com>
2017-04-01 02:29:29 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,31 +1,29 @@
-function callMe() {
+function callMe(){
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
-function crazy() {
-  // fix the code in here:
-
-  var thisIsCrazy
-    thisIsCrazy();
-    thisIsCrazy = function (){
+function crazy(){
+  //fix the code in here:
+  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var sayMy = "Kristin";
+  var name = "Cricky";
 
   sayMy();
 
-  sayMy = function () {
-    console.log(Kristin);
+  function sayMy() {
+    var name = "Kristin";
 
+    console.log(name);
   }
 }
