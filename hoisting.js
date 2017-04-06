ac536e38df748dd69f02821ada924abe077dfa33
Nayeli C <nayelicruz12@gmail.com>
2017-04-06 21:46:55 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -20,11 +20,12 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
+  var name = "Cricky";
 
+  sayMy();
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
 }
-sayMyName();
