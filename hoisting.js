516c31b6ad7c000e3c4ba30be1cf2828a6af920b
Michael Spinnenweber <spincheetah@gmail.com>
2017-04-09 08:52:24 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,17 +1,17 @@
 function callMe() {
-  var lyric = "maybe";
-  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-  var thisIsCrazy;
-  thisIsCrazy = function (){
+
+  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
@@ -24,8 +24,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    var name;
-    name = "Kristin";
+    var name = "Kristin";
     console.log(name);
   }
 }
