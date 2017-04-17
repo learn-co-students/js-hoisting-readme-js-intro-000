f4a29c4bdef6ffbebe52fcd2f2676d76c889bdf8
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-04-17 18:46:13 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,26 +1,28 @@
-var lyric = "maybe";
-function callMe() {
+
+var callMe = function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";
   return lyric;
 }
+callMe();
 
-
-var thisIsCrazy = function crazy() {
+var crazy = function() {
+  var thisIsCrazy = function() {
     console.log("hey!!!");
   }  thisIsCrazy();
 }
+crazy();
 
-function sayMyName() {
-  // fix the code in here:
+var sayMyName = function() {
   var name = "Cricky";
+  function sayMy() {
+    name = "Kristin";
+    console.log(name);
+  }
 
   sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
+sayMyName();
