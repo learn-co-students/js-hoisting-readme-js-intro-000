3d273fa8b1012a100024601afcc937a01e362650
Jessie Baude <jessie.baude@gmail.com>
2017-07-01 14:22:25 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,33 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+function crazy(){
+  console.log();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
+
+
+
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
 
-  function sayMy() {
-    console.log(name);
+
+  function MyName() {
     var name = "Kristin";
+    console.log(name);
   }
+  MyName();
 }
