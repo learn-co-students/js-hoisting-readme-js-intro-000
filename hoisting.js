da2550448fe1caeb14ab7ad278bd3aa93ed15f84
Michael Pagano <m.pagano02@gmail.com>
2017-07-13 23:27:23 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,31 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
 
+  // fix the code in here:
+  function crazy() {
+  var saying = "hey!!!"
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  console.log(saying);
 }
 
-function sayMyName() {
+
   // fix the code in here:
-  var name = "Cricky";
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
+    function sayMyName() {
     var name = "Kristin";
+    var sayMy = function() {
+    console.log("Kristin");
   }
+  console.log(name);
 }
