7f5344ff35d2e229db395a2b6fe8b43bd2eb4907
Kendall Ruggles <kruggles1221@gmail.com>
2017-09-08 02:51:39 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
+var lyric = "maybe";
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
@@ -12,19 +13,18 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy (){
     console.log("hey!!!")
   }
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
   }
 }
