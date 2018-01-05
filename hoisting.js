d9cfb640b147b40f3c61cfff8bc02177316517dd
dariodlearnco <dario.dorella@gmail.com>
2018-01-05 22:04:46 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,32 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+    console.log("I just met you...");
+    console.log("and this is crazy..");
+    console.log("but here's my number..");
+    console.log("so call me");
+    lyric = "maybe";
+    return lyric;
+    var lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+    // fix the code in here:
 
-  var thisIsCrazy = function (){
+    var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
+    }
+    thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+    // fix the code in here:
+    var name = "Cricky";
 
-  sayMy();
+    sayMy();
 
-  function sayMy() {
+    function sayMy() {
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
-  }
+    var name;
+    }
 }
