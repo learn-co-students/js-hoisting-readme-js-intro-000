f3957a2ebedbe234abd159d47bde6e552ee6d212
mark melesse <mark.melesse@gmail.com>
2018-01-15 00:38:34 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,18 +3,18 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  function thisIsCrazy() {
+    console.log("hey!!!");
   }
+  //thisIsCrazy()
 }
 
 function sayMyName() {
@@ -25,6 +25,7 @@ function sayMyName() {
 
   function sayMy() {
     console.log(name);
+    console.log("Kristin")
     var name = "Kristin";
   }
 }
