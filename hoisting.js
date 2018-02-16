4433e7460e525fa1589060a4e67635ce44fc7fe7
Bobby Lucas <bobbylucas622@yahoo.com>
2018-02-16 07:51:27 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,12 @@
-function callMe() {
+function callMe() { 
+var lyric = "maybe";
+  
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
@@ -12,19 +14,21 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+   function thisIsCrazy  (){
+    console.log("hey!!!");
   }
 }
 
 function sayMyName() {
   // fix the code in here:
+  sayMyName(name);
   var name = "Cricky";
 
-  sayMy();
+  
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+  function sayMyName() {
+    var myName;
+  console.log("Kristin");
+    
   }
 }
