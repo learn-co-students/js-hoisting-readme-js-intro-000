5d6328d86846a09a27613e0e7a771a32598747d7
Liangtian Zhang <lzhang40@binghamton.edu>
2018-03-13 16:11:43 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,8 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
@@ -21,10 +21,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
