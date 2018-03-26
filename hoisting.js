35fe616ee116174fc0287f72a63913c1500c4f46
Yung Leung <yungl.leung1@gmail.com>
2018-03-26 01:56:01 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,32 @@
+callMe();
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric;
+  lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  var thisIsCrazy
+  thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
+  var name
+  name = "Cricky";
   function sayMy() {
+    var name
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
+  sayMy();
 }
