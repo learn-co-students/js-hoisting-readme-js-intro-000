91f7e35b74faeb8d5168f0ad02465c8d04085942
Carleen mahon <carleenmahon92@gmail.com>
2018-07-28 19:20:43 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,8 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
@@ -15,16 +15,16 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
