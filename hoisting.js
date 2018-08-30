252a29e5104db27dc58bbe0bce0feb9997246261
Johnathon Wood <jaywood128@gmail.com>
2018-08-30 22:52:11 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,1 +1,29 @@
+function callMe() {
+  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+}
 
+function crazy() {
+  // fix the code in here:
+    var thisIsCrazy = function (){
+    console.log("hey!!!")
+ }
+  thisIsCrazy();
+}
+
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
+
+  sayMy();
+
+  function sayMy() {
+    var name = "Kristen";
+
+    console.log(name);
+  }
+}
