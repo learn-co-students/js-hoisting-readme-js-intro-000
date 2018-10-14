491fedc2e948cff91a398793259127c50ab39abe
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-10-14 08:16:07 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,14 +1,13 @@
 function callMe(){
-  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  var lyric = "maybe";
   return lyric;
 }
 
 function crazy(){
-  //fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
@@ -16,14 +15,11 @@ function crazy(){
 }
 
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
 
   function sayMy() {
     var name = "Kristin";
-
     console.log(name);
   }
+  sayMy();
 }
