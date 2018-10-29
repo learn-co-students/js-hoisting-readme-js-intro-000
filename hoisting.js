db6659d65a480b708ad82f05aae49379e05e7f37
Billy Witherspoon <wwither@gmail.com>
2018-10-29 17:45:00 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -11,16 +11,15 @@ function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  thisIsCrazy();
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
   function sayMy() {
-    console.log(name);
     var name = "Kristin"
-  sayMy();
+    console.log(name);
   }
+  sayMy();
 }
