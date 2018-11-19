5afa5bd9c876736212f0118f5ea1d3312f2ed72c
lanasgoddard <lanasgoddard@gmail.com>
2018-11-19 18:21:05 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,24 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+thisIsCrazy();
+var thisIsCrazy = function crazy() {
     console.log("hey!!!")
   }
 }
 
+
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
+  var name = "Kristin";
+  return function sayMy() {
+    var name = "Cricky";
     console.log(name);
-    var name = "Kristin";
   }
 }
+sayMyName();
