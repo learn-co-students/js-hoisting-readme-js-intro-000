ba8d9bc992be3bfed3728da64304ccfd6e6d12d6
gam funnie <gamfunny@gmail.com>
2018-11-29 07:37:10 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -10,10 +10,12 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
+
+    thisIsCrazy();
   }
 }
 
@@ -21,10 +23,13 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+
+
+    sayMy();
   }
 }
