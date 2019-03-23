15ed4d9a5110c10b806fe6784294b16fcdcf71f8
Tom Durrant <tomchdurrant@gmail.com>
2019-03-23 11:32:14 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -12,8 +12,9 @@ function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-    thisIsCrazy();
   }
+  thisIsCrazy();
+
 }
 
 function sayMyName() {
