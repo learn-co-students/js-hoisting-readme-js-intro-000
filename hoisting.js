32e94d5e97078252bdc49a2e7ebda9765b0a6570
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-04-24 15:24:17 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,22 @@
-function callMe() {
+var lyric, name
+
+function crazy() {
+  console.log("hey!!!");
+}
+
+function callMe(lyric) {
+  lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return(lyric);
 }
+callMe(lyric);
 
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
+crazy();
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
+  name = "Kristin";
     console.log(name);
-    var name = "Kristin";
-  }
 }
