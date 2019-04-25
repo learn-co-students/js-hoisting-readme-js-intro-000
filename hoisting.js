4561a1284aa4b8e4395969259f8c91349e0a6cec
Nick Cooper <nickcooper84@gmail.com>
2019-04-25 20:32:05 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,22 +3,24 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
+
+callMe = lyric
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+  console.log("hey!!!");
 }
+  var thisIsCrazy; //fix the code in here:
+  thisIsCrazy();
+  thisIsCrazy = function(){
+    console.log("hey!!!");
+  }
 
 function sayMyName() {
-  // fix the code in here:
+  console.log("Kristin");// fix the code in here:
   var name = "Cricky";
 
   sayMy();
