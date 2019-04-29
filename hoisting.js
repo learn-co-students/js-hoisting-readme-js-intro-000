eb0390aab3907bba9e086f9792af1faf90251141
Harum Helmy <harumendhah@gmail.com>
2019-04-29 13:23:43 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
@@ -15,10 +15,10 @@ function crazy() {
 }
 
 function sayMyName() {
-  // var name = "Cricky";
+  var name = "Cricky";
+  sayMy();
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
-  sayMy();
 }
