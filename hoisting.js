2dff074b40dac878112be54416ae5e62994ad49b
voluble-block-7446 <iagogomes80@gmail.com>
2019-08-12 17:04:14 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -5,22 +5,17 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+   var thisIsCrazy =function (){
     console.log("hey!!!")
   }
+ return thisIsCrazy;
 }
-
+crazy()();
 function sayMyName() {
-  // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+   sayMyName()();
 }
