e7d03842214b7d14dce62290e58dfc594a363412
Amanda R <abrolon87@gmail.com>
2019-08-12 05:34:40 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,27 @@
+lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
-
 
 function crazy() {
-  // fix the code in here:
+  console.log("hey!!!");
+  var thisIsCrazy = function() {
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!");
   }
 }
 
+var name = "Kristin";
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
+  name = "Cricky";
+  console.log(name)
   sayMy();
-
   function sayMy() {
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
-  }
+ }
 }
