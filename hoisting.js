96ec65a520d179a1f2dc8a6a6ec16565851fcf65
hilarious-programmer-2558 <combatdstar@gmail.com>
2019-08-18 16:26:19 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,35 @@
 function callMe() {
+  var lyric; // hoisting
+  lyric = "maybe";
+  
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  var thisIsCrazy;
+   thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy(); // we can't hoist FUNCTION at the top
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
+  function sayMy() { // inner functions auto hoisted to top
+    var name;
+    name = "Kristin";
+    console.log(name);
+  }
+  
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
