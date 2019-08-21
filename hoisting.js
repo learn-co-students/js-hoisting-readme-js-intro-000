b047f305e1e4f85f57edbee1789490502916f7f9
Abhishek Jadhav <abhidj0001@gmail.com>
2019-08-21 07:01:15 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,55 +1,29 @@
-var callMe = function callMe() {
-
+function callMe(){
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
-  //put var lyric above return lyric
 }
-callMe();
 
-// function crazy() {
-//   // fix the code in here:
-//   thisIsCrazy();
-//
-//   var thisIsCrazy = function (){
-//     console.log("hey!!!")
-//   }
-// }
-
-var crazy = function() {
-  // fix the code in here:
-    var thisIsCrazy = function() {
+function crazy(){
+  //fix the code in here:
+  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
 }
-crazy()
 
-
-// function sayMyName() {
-//   // fix the code in here:
-//   var name = "Cricky";
-//
-//   function sayMy() {
-//     name = "Kristin"; // took out var before name here
-//     console.log(name);
-//   }
-//   sayMy();
-// }
-// sayMyName()()
-
-var sayMyName = function() {
+function sayMyName() {
   // fix the code in here:
-  var name;
-  name = "Cricky";
+  var name = "Cricky";
+
+  sayMy();
 
   function sayMy() {
-    name = "Kristin"; // took out var before name here
+    var name = "Kristin";
+
     console.log(name);
   }
-  sayMy();
 }
-sayMyName()()
