079ae06a505e18da9a3df93c49b1ea145c819bf4
excellent-application-0463 <jeffrey.chan-1@outlook.com>
2019-10-15 07:21:11 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -9,22 +9,20 @@ function callMe() {
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+var name = "Cricky";
+var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+
   }
 }
