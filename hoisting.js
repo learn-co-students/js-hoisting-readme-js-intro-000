ae49f9b89ca8fb601952895f9a68138d2520c011
Tim Kotowski <tkotowski7@gmail.com>
2020-01-13 21:47:12 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,10 @@
 function callMe()  {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
   let lyric = "maybe"
-return lyric;
-
+ return lyric;
 }
 callMe()
 
-
+let
 function thisIsCrazy() {
   // fix the code in here:
     console.log("hey!!!")
@@ -20,7 +15,6 @@ function thisIsCrazy() {
   function sayMyName() {
     // fix the code in here:
     let name = "Cricky";
-
-      console.log(name);
+    console.log(myName);
   }
   sayMyName();
