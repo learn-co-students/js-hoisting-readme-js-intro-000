ff6f0cdc038073395abaafc4392a992c287281d8
Noa'man Patel <normz@live.co.uk>
2020-04-08 16:30:27 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,66 +1,27 @@
 function callMe() {
-var callMe = function callMe() {
-
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
-  return lyric;
-  //put var lyric above return lyric
-}
-callMe();
 
-// function crazy() {
-//   // fix the code in here:
-//   thisIsCrazy();
-//
-//   var thisIsCrazy = function (){
-//     console.log("hey!!!")
-//   }
-// }
+}
 
 function crazy() {
-var crazy = function() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-  var thisIsCrazy = function() {
-    console.log("hey!!!")
+var thisIsCrazy = function (){
+  console.log("hey!!!")
   }
   thisIsCrazy();
-}
-crazy()
+ }
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-// function sayMyName() {
-//   // fix the code in here:
-//   var name = "Cricky";
-//
-//   function sayMy() {
-//     name = "Kristin"; // took out var before name here
-//     console.log(name);
-//   }
-//   sayMy();
-// }
-// sayMyName()()
-
-var sayMyName = function() {
-  // fix the code in here:
-  var name;
-  name = "Cricky";
-
-  function sayMy() {
-    name = "Kristin"; // took out var before name here
-    console.log(name);
-    var name = "Kristin";
-  }
-  sayMy();
-}
-sayMyName()()
+  function sayMyName() {
+    // fix the code in here:
+    var name = "Cricky";
+    function sayMy() {
+      var name = "Kristin";
+      console.log(name);
+      }
+      sayMy();
+      }
