b413a86c8f645d8555a400e10551d699c8a9fa3f
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-09 17:32:27 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,16 +1,17 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+  console.log("hey!!!")// fix the code in here:
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
@@ -18,7 +19,7 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
+  console.log("Kristin");// fix the code in here:
   var name = "Cricky";
 
   sayMy();
