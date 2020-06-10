6b4c339d803eb4a3ad3408fb802c97066aa4eeb9
Kyle Nugent <kln5656@gmail.com>
2020-06-10 21:19:53 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,31 +1,28 @@
-var callMe = function callMe() {
+function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
-callMe();
 
-var crazy = function() {
+function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
       console.log("hey!!!")
   }
   thisIsCrazy();
 }
-crazy();
 
-var sayMyName = function() {
+function sayMyName() {
   // fix the code in here:
-  var name;
-  name = "Cricky";
+  var name = "Cricky";
 
-var sayMy = function() {
-  name = "Kristin";
+  sayMy();
+
+function sayMy() {
+  var name = "Kristin";
   console.log(name);
   }
-  sayMy();
 }
-sayMyName()()
