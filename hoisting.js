c51e45d6c0866217eeb6e2b662589061346b0488
github username <github email address>
2020-12-22 23:20:07 -0500

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,27 @@
+
+callMe();
+var lyric = "maybe";
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
+
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
+
 }
+
 
 function sayMyName() {
   // fix the code in here:
@@ -23,8 +29,10 @@ function sayMyName() {
 
   sayMy();
 
-  function sayMy() {
+  function sayMy(){
+    name = "Kristin";
+
     console.log(name);
-    var name = "Kristin";
   }
+
 }
