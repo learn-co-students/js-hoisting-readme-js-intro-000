cde4a16a113065aa9088c0f42d0f4c3706374a0a
Dilovar Mamadov <d.mamadov@gmail.com>
2021-03-02 03:39:08 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,34 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
+var name = "Cricky";
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = "Kristin";
 
-  sayMy();
+
 
   function sayMy() {
     console.log(name);
-    var name = "Kristin";
+
   }
+  sayMy();
 }
