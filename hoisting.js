b98a242c49cc4e8f9a6a7e2637a7f3df71b12bca
Anri Kawahara <anri0806@hotmail.co.jp>
2021-04-01 03:06:00 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,7 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return "maybe";
 }
 
 
@@ -12,7 +11,7 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
 }
@@ -24,7 +23,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
